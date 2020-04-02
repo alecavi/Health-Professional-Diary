f5639a413c92f593080325e8f51f25e8f8f5424e
ScottyDog5 <55397552+ScottyDog5@users.noreply.github.com>
2020-04-02 16:40:08 +0100

    Added diary

--- src/diary/Diary.java
+++ src/diary/Diary.java
@@ -1,13 +1,59 @@
 package diary;
 
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import change.Change;
+
 /**
- * This class is intentionally left empty. It serves as a mockup, to avoid compiler errors while writing code that
- * will eventually depend on it
- * @author Alessandro Cavicchioli
+ * @author Scott Maclennan
  * @version 1.0
  *
  */
-public class Diary
+public class Diary implements Serializable, Iterable<Entry>
 {
+	private static final long serialVersionUID = 1L;
 	
+	private Map<ZonedDateTime, Entry> diary = new TreeMap<>();
+	private transient Deque<Change> history = new LinkedList<>();
+
+	
+	public void addEntry(Entry newEntry) 
+	{
+		
+		diary.put(newEntry.getStartTime(), newEntry);
+		
+	}
+	
+	public void deleteEntry(Entry entry)
+	{
+		
+		diary.remove(entry.getStartTime());
+		
+	}
+	
+	public void editEntry(Entry oldEntry, Entry newEntry)
+	{
+		diary.put(oldEntry.getStartTime(), newEntry);
+	}
+	
+	public void getEntryByStartDateTime(ZonedDateTime startTime) 
+	{
+		
+		diary.get(startTime);
+		
+	}
+	
+	
+	
+	@Override
+	public Iterator<Entry> iterator() 
+	{
+		return diary.values().iterator();
+	}
 }
