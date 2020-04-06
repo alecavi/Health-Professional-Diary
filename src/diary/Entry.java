08fe0685b662dc369c65927269122c069f36e9a8
Alessandro Cavicchioli <acavicchioli@dundee.ac.uk>
2020-04-06 10:50:22 +0100

    Added javadoc

--- src/diary/Entry.java
+++ src/diary/Entry.java
@@ -3,9 +3,9 @@ package diary;
 import java.time.ZonedDateTime;
 
 /**
- * 
- * @author scott
- *
+ * Represents a single entry (appointment) in a diary
+ * @author Scott Maclennan
+ * @version 1.0
  */
 public class Entry 
 {
@@ -13,6 +13,12 @@ public class Entry
 	private ZonedDateTime endDateTime;
 	private String treatmentType;
 	
+	/**
+	 * Constructs a new entry with the specified parameters
+	 * @param startDateTime the start date and time
+	 * @param endDateTime the end date and time
+	 * @param treatmentType the treatment type
+	 */
 	public Entry(ZonedDateTime startDateTime, ZonedDateTime endDateTime, String treatmentType) 
 	{
         this.startDateTime = startDateTime;
@@ -20,24 +26,37 @@ public class Entry
         this.treatmentType = treatmentType;
 	}
     
+	/**
+	 * @return the start date and time
+	 */
     public ZonedDateTime getStartDateTime()
     {
         return startDateTime;
     }
     
+    /**
+     * @return the end date and time
+     */
     public ZonedDateTime getEndDateTime()
     {
         return endDateTime;
     }
     
+    /**
+     * @return the treatment type
+     */
     public String getTreatmentType()
     {
         return treatmentType;
     }
     
+    /**
+     * Returns a human-readable string representation of the contents of this entry
+     * @return a string representing the contents of this entry
+     */
     public String getInfo() 
     {
-    	return startDateTime + " " + endDateTime + " " + treatmentType;
+    	return startDateTime + "-" + endDateTime + "," + treatmentType;
     }
     
 }
